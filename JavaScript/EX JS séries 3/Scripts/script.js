3734a26a555cdbbe72398a292a9787194e413f31
Alexlcr <34378196+Alexlcr@users.noreply.github.com>
2025-05-22 09:46:42 +0200

    update

--- JavaScript/EX JS séries 3/Scripts/script.js
+++ JavaScript/EX JS séries 3/Scripts/script.js
@@ -1,84 +1,94 @@
-// //EX1
-// function len(str) {
-//     return str.length;
-// }
+//EX1
+console.log("####### EXERCICE 1 #######");
 
-// //EX2
+function len(str) {
+    return str.length;
+}
 
-// function isPresentIn(tab, element) {
-//     if (element in tab)
-//         return true;
-//     else
-//         return false;
-// }
+//EX2
+console.log("####### EXERCICE 2 #######");
 
-// //EX3
+function isPresentIn(tab, element) {
+    if (element in tab)
+        return true;
+    else
+        return false;
+}
 
-// function lenTab(tab) {
-//     return tab.length;
+//EX3
+console.log("####### EXERCICE 3 #######");
+
+function lenTab(tab) {
+    return tab.length;
     
-// }
+}
 
-// //EX4
+//EX4
+console.log("####### EXERCICE 4 #######");
 
-// let textEX4 = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, magni."
-// let textEX4Split = textEX4.split(" ");
-// let tabEX4 = [];
+let textEX4 = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, magni."
+let textEX4Split = textEX4.split(" ");
+let tabEX4 = [];
 
-// for (i in textEX4Split) {
-//     tabEX4.push(textEX4Split[i]);
-// }
-// console.log(tab);
+for (i in textEX4Split) {
+    tabEX4.push(textEX4Split[i]);
+}
+console.log(tabEX4);
 
-// //EX5
-// tabEX5 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-// valeur = 5;
-// function filterNombres(tab, valeur) {
-//     let tabFiltre = [];
-//     for (i in tab){
-//         if (tabEX5[i] > valeur)
-//             tabFiltre.push(tab[i]);
-//     }
-//     return tabFiltre;
-// }
+//EX5
+console.log("####### EXERCICE 5 #######");
+tabEX5 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+valeur = 5;
+function filterNombres(tab, valeur) {
+    let tabFiltre = [];
+    for (i in tab){
+        if (tabEX5[i] > valeur)
+            tabFiltre.push(tab[i]);
+    }
+    return tabFiltre;
+}
 
-// console.log(filterNombres(tabEX5, valeur));
+console.log(filterNombres(tabEX5, valeur));
 
-// //EX6
-// class personne{
-//     constructor(nom) {
-//         this.nom = nom;
-//     }
-// }
+//EX6
+console.log("####### EXERCICE 6 #######");
+class personne{
+    constructor(nom) {
+        this.nom = nom;
+    }
+}
 
-// function addProp(objet, cle, valeur) {
-//     return objet[cle] = valeur
-// }
-// addProp(personne, "nom", "John");
-// addProp(personne, "age", "25");
+function addProp(objet, cle, valeur) {
+    return objet[cle] = valeur
+}
+addProp(personne, "nom", "John");
+addProp(personne, "age", "25");
 
-// console.log(personne.nom);
-// console.log(personne.age);
+console.log(personne.nom);
+console.log(personne.age);
 
-// //EX7
-// let tabEX7 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-// let extendTabEX7 = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+//EX7
+console.log("####### EXERCICE 7 #######");
+let tabEX7 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let extendTabEX7 = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 
-// for (i in tabEX7) {
-//     tabEX7.push(extendTabEX7[i]);
-// }
-// console.log(tabEX7);
+for (i in tabEX7) {
+    tabEX7.push(extendTabEX7[i]);
+}
+console.log(tabEX7);
 
-// //EX8
-// let tabEX8 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+//EX8
+console.log("####### EXERCICE 8 #######");
+let tabEX8 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-// function reverseTab(tab) {
-//     return tab.reverse();
-// }
+function reverseTab(tab) {
+    return tab.reverse();
+}
 
-// console.log(reverseTab(tabEX8));
+console.log(reverseTab(tabEX8));
 
 //EX9
+console.log("####### EXERCICE 9 #######");
 let tabEX9 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 function extract(tab, born1, born2) {
@@ -91,3 +101,38 @@ function extract(tab, born1, born2) {
 }
 
 console.log(extract(tabEX9, 2, 5));
+
+//EX10
+console.log("####### EXERCICE 10 #######");
+
+let strEX10 = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, magni."
+
+function remplcerMot(str, ancienMot, nouveauMot){
+    return str.replace(ancienMot, nouveauMot);
+}
+
+strEX10 = remplcerMot(strEX10, "Lorem", "meroL");
+console.log(strEX10);
+
+
+// EX BONUS PAS DANS LA CONSIGNE MAIS ÇA ME FAIT RIRE D'ESSAYER
+let strBonus = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, magni."
+
+function fonctionBonus(strBonus){
+
+    let scraped = [];
+    let ancienMot = [];
+    let nouveauMot;
+    let splitStrBonus = strBonus.split(" ");
+
+    for (i in splitStrBonus) {
+        ancienMot = splitStrBonus[i];
+        scraped = ancienMot.split("");
+        nouveauMot = (reverseTab(scraped)).join("");
+        strBonus = strBonus.replace(ancienMot, nouveauMot);
+    }
+
+    return strBonus;
+    
+}
+console.log(fonctionBonus(strBonus));
---
+++ JavaScript/EX JS séries 4/scripts/script.js
@@ -0,0 +1,19 @@
+//EX 1
+let tabEX1 = [1, 2, 3, 4, 5];
+for (i in tabEX1) {
+    console.log(tabEX1[i]);
+}
+
+//EX 2
+let tabEX2 = [1, 2, 3, 4, 5];
+let somme = 0;
+for (i in tabEX2) {
+    somme = somme + tabEX2[i];
+    }
+console.log(somme);
+
+//EX 3
+let strEX3 = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, magni."
+function longestWord(str){
+    
+}
